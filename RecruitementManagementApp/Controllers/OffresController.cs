982e51fdcb334a40b07e81ee603feadb71bf18a6
hadil30 <hedilfares30@gmail.com>
2023-12-20 14:52:41 +0100

    FilterbyTitleandType

--- RecruitementManagementApp/Controllers/OffresController.cs
+++ RecruitementManagementApp/Controllers/OffresController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
+using Humanizer.Localisation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration.UserSecrets;
 using RecruitementManagementApp.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace RecruitementManagementApp.Controllers
 {
@@ -21,18 +24,199 @@ namespace RecruitementManagementApp.Controllers
 
 
 
-        
+
+        public async Task<IActionResult> FilterOffre(string searchString)
+        {
+            var alloffres = await _context.Offres.Include(o => o.LeRh).ToListAsync();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var filteredResult = alloffres.Where(n => n.Title.ToLower().Contains(searchString.ToLower()) || n.Description.ToLower().Contains(searchString.ToLower())).ToList();
+                return View("Index", filteredResult);
+            }
+
+            return View("Index", alloffres);
+        }
 
 
-        // GET: Offres
-        public async Task<IActionResult> Index()
+       /* public async Task<IActionResult> Index()
         {
             var userId = HttpContext.Session.GetInt32("UserId");
 
             var recruitementDbContext = _context.Offres.Where(o => o.nameRh == userId.Value);
-          
+
+            return View(await recruitementDbContext.ToListAsync());
+        }*/
+
+
+
+        public IActionResult Index(string titre, string type)
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            IQueryable list = _context.Offres.Include(o => o.LeRh).Where(o => o.nameRh == userId.Value);
+            var lestypes = new List<string> { "Publier", "Archiver" };
+            ViewBag.type = new SelectList(lestypes);
+
+            if (titre != null && type != null)
+            {
+                if (type == "Publier")
+                {
+                    list = from m in _context.Offres.Include(o => o.LeRh).Where(o => o.nameRh == userId.Value && o.Title.Contains(titre)
+                           && o.published == true)
+                           select m;
+
+                }
+                else
+                {
+                    list = from m in _context.Offres.Include(o => o.LeRh).Where(o => o.nameRh == userId.Value && o.Title.Contains(titre)
+                           && o.archived == true)
+                           select m;
+
+                }
+
+            }
+            else if (titre == null && type == null)
+            {
+                list = from m in _context.Offres.Include(o => o.LeRh).Where(o => o.nameRh == userId.Value)
+                       select m;
+            }
+
+            else if (titre == null)
+            {
+                if (type == "Publier")
+                {
+                    list = from m in _context.Offres.Include(o => o.LeRh).Where(o => o.nameRh == userId.Value
+                           && o.published == true)
+                           select m;
+
+                }
+                else
+                {
+                    list = from m in _context.Offres.Include(o => o.LeRh).Where(o => o.nameRh == userId.Value
+                           && o.archived == true)
+                           select m;
+
+                }
+            }
+            else if (type == null)
+            {
+                list = from m in _context.Offres.Include(o => o.LeRh)
+                       where m.nameRh == userId.Value && m.Title.Contains(titre)
+                       select m;
+            }
+            return View(list);
+        }
+
+
+
+        /*
+        public IActionResult SearchBy2(string title, string type )
+        {
+
+
+            var userId = HttpContext.Session.GetInt32("UserId");
+            IQueryable list = _context.Offres.Where(o => o.nameRh == userId.Value);
+
+
+
+
+
+            if (title != null && type != null)
+            {
+
+                if (type == "published")
+                {
+                    /*where m.Title.Contains(titre)
+                       && m.Genre.Contains(genre)*/
+        /* list = from o in _context.Offres.Where(o => o.nameRh == userId.Value && o.published && o.Title.Contains(title)) 
+                select o;
+     }
+     else if (type == "archived")
+     {
+
+         list = from o in _context.Offres.Where(o => o.nameRh == userId.Value && o.archived && o.Title.Contains(title))
+                select o;
+     }
+ }
+ else if (title == null && type == null)
+ {
+     list = from o in _context.Offres.Where(o => o.nameRh == userId.Value)
+     select o;
+ }
+ else if (type == null)
+ {
+     list = from m in _context.Offres.Where(o => o.nameRh == userId.Value)
+     where m.Title.Contains(title)
+            select m;
+ }
+
+
+ return View(list);
+
+}
+
+*/
+        /*
+        public async Task<IActionResult> FilterAchrived(string searchString)
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            var alloffres = await _context.Offres.Where(o => o.nameRh == userId.Value && o.archived == true).ToListAsync();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var filteredResult = alloffres.Where(n => n.Title.ToLower().Contains(searchString.ToLower()) || n.Description.ToLower().Contains(searchString.ToLower())).ToList();
+                return View("Index", filteredResult);
+            }
+
+            return View("Index", alloffres);
+        }
+
+
+        public async Task<IActionResult> AchivedIndex()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+
+            var recruitementDbContext = _context.Offres.Where(o => o.nameRh == userId.Value && o.archived==true);
+
             return View(await recruitementDbContext.ToListAsync());
         }
+
+        public async Task<IActionResult> FilterPublished(string searchString)
+
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            var alloffres = await _context.Offres.Where(o => o.nameRh == userId.Value && o.archived == true).ToListAsync();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var filteredResult = alloffres.Where(n => n.Title.ToLower().Contains(searchString.ToLower()) || n.Description.ToLower().Contains(searchString.ToLower())).ToList();
+                return View("Index", filteredResult);
+            }
+
+            return View("Index", alloffres);
+        }
+
+        public async Task<IActionResult> PublishedIndex()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+
+            var recruitementDbContext = _context.Offres.Where(o => o.nameRh == userId.Value && o.published == true);
+
+            return View(await recruitementDbContext.ToListAsync());
+        }
+
+
+        
+        */
+
+
+
+
+
+
+
+        // GET: Offres
+
 
         // GET: Offres/Details/5
         public async Task<IActionResult> Details(int? id)
